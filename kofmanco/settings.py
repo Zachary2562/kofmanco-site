445ad6219d646aef3bb05f395fd4a7825463dcba
Zachary2562 <zachary.kofman2@gmail.com>
2025-07-03 14:25:56 -0400

    Update settings.py

--- kofmanco/settings.py
+++ kofmanco/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&z1v(+o=m+=3!74890rlxw+mrxnpx&sn+5mv0!3)irzzy1e0p=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["kofmanco.com"]
+ALLOWED_HOSTS = ["kofmanco.com", "kofmanco.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
